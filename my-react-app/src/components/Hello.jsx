a5e138f44df7b233475b6ef15542ec09b2bf72d7
rafaelbeltz02 <rafaelbeltz947@example.com>
2025-12-09 23:00:34 +0800

    Delete Hello.jsx

--- my-react-app/src/components/Hello.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/Hello.jsx
-export default function Hello() {
-  return (
-    <div style={{padding:20}}>
-      <h2>Hello from React!</h2>
-      <p>This is your first functional component.</p>
-    </div>
-  );
-}
