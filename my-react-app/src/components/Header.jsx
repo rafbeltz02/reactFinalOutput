c029d83d4a6aef5e628236473ccbb1ae8eeba450
rafaelbeltz02 <rafaelbeltz947@example.com>
2025-12-10 00:35:08 +0800

    Create Header.jsx

---
+++ my-react-app/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+  return(
+    <header style={{padding:20, background:"#1a2a3a", color:"#fff"}}>
+      <h2>📘 Introduction to React — Summary</h2>
+    </header>
+  );
+}
