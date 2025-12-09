21c523ba49ce365e5d6accf22969f32604243cbc
rafaelbeltz02 <rafaelbeltz947@example.com>
2025-12-10 01:05:25 +0800

    Update main.jsx

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,13 +1,3 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -18,4 +8,3 @@ createRoot(document.getElementById("root")).render(
     <App />
   </BrowserRouter>
 );
-
