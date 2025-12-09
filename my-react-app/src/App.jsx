9d2def7c2bd7ad6a924dad43d927ef7c5d270717
rafaelbeltz02 <rafaelbeltz947@example.com>
2025-12-09 23:01:30 +0800

    Update App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,14 @@
-// src/App.jsx
-import Hello from "./components/Hello";
+import Car from "./components/Car";
+import LoginForm from "./components/LoginForm";
+import SignUpForm from "./components/SignUpForm";
 
-export default function App() {
+export default function App(){
   return (
     <div>
-      <h1>Challenge Activity 1</h1>
-      <Hello />
+      <h1>Challenge 1 Output</h1>
+      <Car />
+      <LoginForm />
+      <SignUpForm />
     </div>
   );
 }
