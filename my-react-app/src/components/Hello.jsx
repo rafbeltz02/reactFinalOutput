8f886ba1fb0d55249e5b1c3c3496090aa6a3a02c
rafaelbeltz02 <rafaelbeltz947@example.com>
2025-12-09 22:41:53 +0800

    Create Hello.jsx

---
+++ my-react-app/src/components/Hello.jsx
@@ -0,0 +1,9 @@
+// src/components/Hello.jsx
+export default function Hello() {
+  return (
+    <div style={{padding:20}}>
+      <h2>Hello from React!</h2>
+      <p>This is your first functional component.</p>
+    </div>
+  );
+}
