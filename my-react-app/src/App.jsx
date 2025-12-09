ff4a826791610be757381ace5884744b2daaeb4f
rafaelbeltz02 <rafaelbeltz947@example.com>
2025-12-10 01:01:15 +0800

    Update App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -13,6 +13,29 @@ import SignUp_MUI from "./components/SignUp_MUI";
 // Activity 3 Summary Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LoginForm_MUI from "./components/LoginForm_MUI";
+import SignUp_MUI from "./components/SignUp_MUI";
+import Profile from "./components/Profile";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<LoginForm_MUI />} />           {/* default page */}
+        <Route path="/signup" element={<SignUp_MUI />} />
+        <Route path="/profile" element={<Profile />} />          {/* profile page */}
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
 
 export default function App() {
   return (
